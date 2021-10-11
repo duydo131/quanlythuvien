db8d6af5183f366379f102bc34c5880c4b79e72f
duydo131 <dotheduybk131@gmail.com>
2021-10-11 21:13:50 +0700

    dd

--- connect/Sql.java
+++ connect/Sql.java
@@ -5,7 +5,7 @@ public class Sql {
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	public static final String URL_DATABASE = "jdbc:mysql://localhost:3306/quanlythuvien_taintd";
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "dtDptTdvB0";
+	public static final String PASSWORD = "";
 
 
    }
